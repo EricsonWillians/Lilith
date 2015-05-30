99c27185d700c10e7e2e388580186524a12586af
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-05-30 01:31:17 -0300

    Uploaded.

---
+++ nompiler.py
@@ -0,0 +1,31 @@
+"""
+[[GNU]
+
+[[[[[
+[[[[NASTY
+[[[[OVERWHELMING
+[[[[-
+[[[[PANDEMONIUM
+[[[[INFERNAL
+[[[[THOUGHTLESS
+[[[[YGRITTE: YOU KNOW NOTHING, PYTHON!
+[[[[-
+[[[[OVERNASTY
+[[[[NONSENSE
+[[[[]
+
+[GNU]]
+"""
+
+import sys
+
+if __name__ == "__main__":
+	
+	if len(sys.argv) == 2:
+		PATH = sys.argv[1]
+	else:
+		raise RuntimeError("[DO YOU REALLY THINK YOU CAN NOMPILE SOMETHING WITHOUT A PATH??? [G][E][N][I][U][S][!]]")
+		
+	class Nonpiler(whatever)
+		"I'll continue..."
+		pass
