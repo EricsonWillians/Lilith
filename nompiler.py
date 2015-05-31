9fda581742e73a91e6479c70da65d0af0dcabf60
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-05-31 04:05:28 -0300

    [o] added (Print).

--- nompiler.py
+++ nompiler.py
@@ -36,18 +36,21 @@ class Nonpiler():
 		if self.lines[0] != '[' or self.lines[len(self.lines)-1] != ']':
 			raise SyntaxError("[Bad structure... [:(]]")
 		else:
-			for i in range(1, len(self.lines)-2):
-				if self.lines[i] not in self.keywords:
+			for i in range(1, len(self.lines)-1):
+				if '=' in self.lines[i]:
 					self.separate = self.lines[i].replace(' ', '').split('=')
+				elif '<-' in self.lines[i]:
+					self.separate = self.lines[i].replace(' ', '').split('<-')
+				if self.separate[0] not in self.keywords:
 					self.variables[self.separate[0]] = eval(self.separate[1], globals(), self.variables)
-			
+				elif self.separate[0] in self.keywords:
+					if self.separate[0] == '[o]':
+						print(eval(self.separate[1], globals(), self.variables))
+						
 if __name__ == "__main__":
 	
 	if len(sys.argv) == 2:
 		PATH = sys.argv[1]
 		np = Nonpiler(PATH)
-		print(np.variables)
 	else:
 		raise RuntimeError("[Do you really think you can nonpile something without a path??? [G][E][N][I][U][S][!]]")
-	
-	
