03a3f9b23f422e243aca6d31def475e27efb5ea3
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-06-03 02:05:26 -0300

    __expressions__ and __parser__ were added.

--- compiler.py
+++ compiler.py
@@ -1,46 +1,28 @@
 import sys
 import os
-						
+
+class __expressions__(list):
+	
+	def __init__(self):
+		list.__init__(self)
+		
+	def __getitem__(self, index=0):
+		return self.pop
+
+class __parser__():
+	
+	def __init__(self):
+		if len(sys.argv) == 2:
+			self.path = os.path.join(sys.argv[1])
+			with open(self.path, "r") as fuk:
+				self.data = fuk.read().replace('\n', '')
+		else:
+			raise RuntimeError("You must specify a path to a .fuk file.")
+
+	def __str__(self):
+		return self.data
+
 if __name__ == "__main__":
 	
-	__keywords__ = ['[i]', '[o]', '[f]']
-	__operators__ = ['<-', '->']
-	__statements__ = {"Assignment": [], "Output": []}
-	__vars__ = {}
+	print(__parser__())
 	
-	if len(sys.argv) == 2:
-		path = os.path.join(sys.argv[1])
-		with open(path, "r") as fuk:
-			fukdata = fuk.read().replace('\n', '')
-	else:
-		raise RuntimeError("You must specify a path to a .fuk file.")
-
-	def fetch_statements():
-		lines = fukdata.split('->')
-		for line in lines:
-			for operator in __operators__:
-				if operator in line and operator != '->':
-					split_line = line.split(operator)
-					left_side = split_line[0].replace(' ', '')
-					if left_side not in __keywords__:
-						__statements__["Assignment"].append(split_line)
-					elif left_side in __keywords__:
-						if left_side == '[o]':
-							__statements__["Output"].append(split_line)
-						elif left_side == '[i]':
-							__statements__["Output"].append(split_line)
-	
-	def fetch_vars():
-		for var in __statements__["Assignment"]:
-			name = var[0].replace(' ', '')
-			expression = var[1]
-			__vars__[name] = eval(expression, globals(), __vars__)
-			
-	def execute_io_operations():
-		for data in __statements__["Output"]:
-			expression = data[1]
-			print(eval(expression, globals(), __vars__))
-
-	fetch_statements()
-	fetch_vars()
-	execute_io_operations()
