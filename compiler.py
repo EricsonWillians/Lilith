5103b2a221e4ce3c231c91ad2c48fef45a76c0ef
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2015-06-03 02:21:21 -0300

    Tokens added.

--- compiler.py
+++ compiler.py
@@ -1,6 +1,35 @@
 import sys
 import os
 
+class __tokens__(dict):
+	
+	def __init__(self):
+		dict.__init__(self)
+		self['shift_operator'] = '<-'
+		self['addition_shift_operator'] = '<+>'
+		self['subtraction_shift_operator'] = '<->'
+		self['multiplication_shift_operator'] = '<*>'
+		self['division_shift_operator'] = '</>'
+		self['modulus_shift_operator'] = '<%>'
+		self['open_function_identifier'] = '['
+		self['close_function_identifier'] = ']'
+		self['end_of_statement'] = '|'
+		self['open_block'] = '|>'
+		self['close_block'] = '<|'
+		self['if'] = '?'
+		self['then'] = '!>'
+		self['else_if'] = '??'
+		self['else'] = '???'
+		self['negation'] = '!' 
+		self['while'] = '@' 
+		self['for'] = '<@>'
+		self['increment'] = '++' 
+		self['decrement'] = '--' 
+		self['break'] = '<@'
+		self['continue'] = '@>'
+		self['separator'] = '{}'
+		self['return'] = '/\\'
+
 class __expressions__(list):
 	
 	def __init__(self):
@@ -18,11 +47,14 @@ class __parser__():
 				self.data = fuk.read().replace('\n', '')
 		else:
 			raise RuntimeError("You must specify a path to a .fuk file.")
+		
+		self.tokens = __tokens__()
+		self.expressions = __expressions__()
 
 	def __str__(self):
 		return self.data
 
 if __name__ == "__main__":
 	
-	print(__parser__())
+	print(__parser__().tokens)
 	
